8a5d4594f708099877eeeedf879178900a80a908
Beta-23 <alfred.dominguez@gmail.com>
2016-03-07 15:35:21 -0800

    adds create user schema

--- models/user.js
+++ models/user.js
@@ -3,6 +3,32 @@ var mongoose = require('mongoose'),
 Schema = mongoose.Schema,
 bcrypt = require('bcrypt');
 
+// create user schema
+var	UserSchema = new Schema({
+	email:{type: String, required: true},
+	passwordDigest: {type: String, required: true},
+	createdAt: {type: Date, default: Date.now}
+});
+
+// create a new user with secure (hashed) password (for sign up)
+UserSchema.statics.createSecure = function (email, password, cb){
+	// `_this` now references our schema
+	var _this = this;
+	// generate some salt
+  bcrypt.genSalt(function (err, salt) {
+    // hash the password with the salt
+    bcrypt.hash(password, salt, function (err, hash) {
+      // build the user object
+      var user = {
+        email: email,
+        passwordDigest: hash
+      };
+      // create a new user in the db with hashed password and execute the callback when done
+      _this.create(user, cb);
+    });
+  });
+};
+
 // the user schema
 var UserSchema = new Schema({
 	email: {type: String, required: true},
