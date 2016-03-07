c844eed558842d4a7875e3dead87e05936613b7a
Beta-23 <alfred.dominguez@gmail.com>
2016-03-07 15:01:17 -0800

    adds userSchema to model

---
+++ models/user.js
@@ -0,0 +1,11 @@
+//dependencies
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema,
+bcrypt = require('bcrypt');
+
+// the user schema
+var UserSchema = new Schema({
+	email: {type: String, required: true},
+	passwordDigest: {type: String, require: true},
+	createdAt: {type: Date, default: Date.now()}
+});
