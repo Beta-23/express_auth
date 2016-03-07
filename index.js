64ad6dde4d5d039a04d85d49a5de441d4ec7f0d9
Ben Hulan <benhulan@gmail.com>
2016-03-06 17:54:41 -0800

    first six steps

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    db = require('./models'),
+    session = require('express-session'),
+    path = require('path'),
+    app = express();
+
+var views = path.join(process.cwd(), 'views');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(
+    session({
+        secret: 'super-secret-private-keyyyyy',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+app.use(function(req, res, next){
+    req.login = function(user){
+        req.session.userId = user._id;
+    };
+    req.currentUser = function(callback){
+        db.User.findOne({_id: req.session.userId},
+            function(err, user){
+                req.user = user;
+                callback(null, user);
+            });
+    };
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(path.join(views, 'login.html'));
+});
+
+app.get('/signup', function(req, res){
+    res.send('Coming soon');
+});
+
+app.post(['/users', '/signup'], function signup(req, res){
+    var user = req.body.user;
+    var email = user.email;
+    var password = user.password;
+    db.User.createSecure(email, password, function(){
+        res.send(email + ' is registered!\n');
+    });
+});
+
+app.post(['/sessions', '/login'], function login(req, res){
+    var user = req.body.user;
+    var email = user.email;
+    var password = user.password;
+    db.User.authenticate(email, password, function(err, user){
+        // res.send(email + ' is logged in\n');
+        req.login(user);
+        res.redirect('/profile');
+    });
+});
+
+app.get('/profile', function userShow(req, res){
+    req.currentUser(function(err, user){
+        res.send('Hello ' + user.email);
+    });
+});
+
+app.listen(3000, function(){
+    console.log('listening on port 3000');
+});
---
+++ models/user.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt');
+
+var UserSchema = new Schema({
+    email: {type: String, required: true},
+    passwordDigest: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now()}
+});
+
+UserSchema.statics.createSecure = function(email, password, callback){
+    var _this = this;
+    bcrypt.genSalt(function(err, salt){
+        bcrypt.hash(password, salt, function (err, hash){
+            var user = {
+                email: email,
+                passwordDigest: hash
+            };
+            _this.create(user, callback);
+        });
+    });
+};
+
+
+UserSchema.statics.authenticate = function(email, password, callback){
+    this.findOne({email: email}, function(err, user){
+        if(user === null) {
+            callback('Cannot find user with that email', null);
+        } else if (user.checkPassword(password)) {
+            callback(null, user);
+        } else {
+            callback('password incorrect', user);
+        }
+    });
+};
+
+UserSchema.methods.checkPassword = function(password){
+    return bcrypt.compareSync(password, this.passwordDigest);
+};
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
