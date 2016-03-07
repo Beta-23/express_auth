1b69856ae76ba4220163d88bc241e0554a48b912
Beta-23 <alfred.dominguez@gmail.com>
2016-03-07 14:38:14 -0800

    adds port listener to index

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.use(bodyParser.unlencode({extended: true}));
 app.get("/signup", function (req, res){
 	res.send("Coming soon");
 });
+
+var listener = app.listen(3000, function(){
+	console.log("Listening on port " + listener.address().port);
+});
